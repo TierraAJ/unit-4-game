7585fe3b3166eb76ee3b0ebbc74cd5bc136216d2
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-08-19 01:36:22 -0400

    adding some css and picture buttons

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+//Set initial variables.
+var computerNumText = document.getElementById("computer-num")
+var userTotalText = document.getElementById("user-total")   
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var wins = 0;
+var losses = 0;
+
+//Generate computer's random number.
+function computerRandom() {
+    var random = Math.floor((Math.random() * 100) + 2);
+    return random
+}
+
+computerNumText = computerRandom()
+console.log(computerNumText)
+
+$("#computer-num").text()
+
+//Assign crystals a random number.
+
+
+//Crystal Onclick Function.
+
+
+//Add additional Clicks to value of first.
+
+
+//Set up if statements to define wins an losses.
+
+
+//
