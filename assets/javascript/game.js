512f57dd31012e84a8e71b1e23c8ee16e2f3663a
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-08-19 11:25:10 -0400

    trying to get buttons to function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,22 +5,37 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var wins = 0;
 var losses = 0;
+var yourTotal = 0;
+var crystalsRandom = [1, 3, 5, 10, 12, 20]
 
 //Generate computer's random number.
 function computerRandom() {
     var random = Math.floor((Math.random() * 100) + 2);
-    return random
+    return random;
 }
 
-computerNumText = computerRandom()
-console.log(computerNumText)
+computerNumText = computerRandom();
+console.log(computerNumText);
 
-$("#computer-num").text()
+$("#computer-num").text();
 
 //Assign crystals a random number.
+function crystal1Random() {
+    var random = Math.floor((Math.random() * crystalsRandom.length));
+    return crystalsRandom[random];
+}
+
+$("#button-1").text() = crystal1Random();
+console.log(crystal1Random);
+
 
 
 //Crystal Onclick Function.
+$(this).onkeyup = function(event) {
+    // Prints key that user selects.
+    $(yourTotal).text(crystal1Random) = event.key;
+    
+  }; 
 
 
 //Add additional Clicks to value of first.
