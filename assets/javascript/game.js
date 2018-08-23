244baa3597d134a2c7016cf7235b73157c496a88
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-08-23 04:15:37 -0400

    trying to get crystal values to display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,28 +20,34 @@ computerNumText = computerRandom();
 console.log("Computer Target Number: " + computerNumText);
 
 //Assign crystals a random number.
-function crysRandom() {
+function crysNum() {
     var crystal1 = Math.floor((Math.random() * 3) + 1);
     var crystal2 = Math.floor((Math.random() * 5) + 1);
     var crystal3 = Math.floor((Math.random() * 10) + 2);
     var crystal4 = Math.floor((Math.random() * 10) + 2);
-    }
-    crysRandom();
-    //Log out random crystal values in Console.
     console.log("Crystal 1: " + crystal1);
     console.log("Crystal 2: " + crystal2);
     console.log("Crystal 3: " + crystal3);
     console.log("Crystal 4: " + crystal4);
+}
+ crysNum()  
+
+$("user-total").on("click", function(event) {
+
+    $(".crystals").append(crystal);
+
+    //Log out random crystal values in Console.
+
 
 userTotalText = yourTotal; 
 $(userTotalText).text("#user-total");    
 
-
-
 //Crystal Onclick Function.
-$(".crystal-icon").click(function(event){
+// $(".crystal-icon").click(function(event){
+    $(this).append("#user-total");
+    //crysNum + yourTotal;
     // Prints key that user selects.
-    $(userTotalText).text("#user-total");
+    //$("#user-total").append(userTotalText);
     
   }); 
 
